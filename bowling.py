d6d5b4dbdfac386e880c23e9f34cdf34c50c011f
Balint Peter <balint.peter23@gmail.com>
2017-11-30 13:34:48 +0100

    shortened lower and upper x in IF

--- bowling.py
+++ bowling.py
@@ -10,7 +10,7 @@ def score(game):
         if frame < 10 and get_value(game[i]) == 10:
             if game[i] == '/':
                 result += get_value(game[i + 1])
-            elif game[i] == 'X' or game[i] == 'x':
+            elif game[i].lower() == 'x':
                 result += get_value(game[i + 1])
                 if game[i + 2] == '/':
                     result += 10 - get_value(game[i + 1])
@@ -23,7 +23,7 @@ def score(game):
             in_first_half = False
         else:
             in_first_half = True
-        if game[i] == 'X' or game[i] == 'x':
+        if game[i].lower() == 'x':
             in_first_half = True
             frame += 1
     return result
